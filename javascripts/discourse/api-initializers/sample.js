8f5485e0b06033647152e8a790e06e10abc18600
Alec McGail <amcgail2@gmail.com>
2024-11-26 10:03:24 -0600

    hiding channels, and logging user info

--- javascripts/discourse/api-initializers/sample.js
+++ javascripts/discourse/api-initializers/sample.js
@@ -2,4 +2,5 @@ import { apiInitializer } from "discourse/lib/api";
 
 export default apiInitializer("1.8.0", (/* api */) => {
   console.log("hello world from api initializer!");
+  console.log(api.getCurrentUser());
 });
