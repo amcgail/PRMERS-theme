18784435d79fd50db5122067d53a68cb383c70c8
Alec McGail <amcgail2@gmail.com>
2024-11-26 10:04:20 -0600

    whoops

--- javascripts/discourse/api-initializers/sample.js
+++ javascripts/discourse/api-initializers/sample.js
@@ -1,6 +1,6 @@
 import { apiInitializer } from "discourse/lib/api";
 
-export default apiInitializer("1.8.0", (/* api */) => {
+export default apiInitializer("1.8.0", (api) => {
   console.log("hello world from api initializer!");
   console.log(api.getCurrentUser());
 });
